51efb2bec8e882e356caa1343ea29cef3cd8cdc5
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-08-08 03:13:55 -0700

    [feat] mongoURL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 export default {
-  mongoURI: "mongodb://127.0.0.1:27017/telegramBot_BD",
+  // mongoURI: "mongodb://127.0.0.1:27017/telegramBot_BD",
+  mongoURI:
+    "mongodb+srv://innovativetech0926:twhPbLJUxWHXB8Vx@innotech.5svyawa.mongodb.net/NinjaStudio",
   jwtSecret: "kut10developers",
   githubToken: "<yoursecrectaccesstoken>",
 };
