ef36809477060121193b481e0e17ab33fb0f3fd3
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-08-09 21:07:31 +0200

    [feat] add a friend funcs

--- src/routes/api/friend.ts
+++ src/routes/api/friend.ts
@@ -1,32 +1,51 @@
 import express, { Router, Request, Response } from "express";
 import mongoose from "mongoose";
 import Friend from "../../models/Friend";
+import Wallet from "../../models/Wallet";
 
 const router: Router = express.Router();
 
-router.post('/add', async (req: Request, res: Response) => {
-    const friend_new = new Friend({
-        username: req.body.username,
-        friend: req.body.friend
-    })
-    try {
-        let friend_check = await Friend.findOne({ friend: req.body.friend });
-        if (friend_check) {
-            return res.status(400).json({ msg: "You are already added in friend item" });
-        } else {
-            await friend_new.save();
-            res.json(friend_new);
-        }
-    } catch (error) {
-        res.status(400).json({ msg: error });
-    }
-})
-router.post("/:username", async (req: Request, res: Response) => {
-    let friend = await Friend.find({ username: req.params.username });
-    if (friend) {
-        res.json(friend);
+router.post("/add", async (req: Request, res: Response) => {
+  const friend_new = new Friend({
+    username: req.body.username,
+    friend: req.body.friend
+  });
+  try {
+    let friend_check = await Friend.findOne({ friend: req.body.friend });
+    if (friend_check) {
+      return res
+        .status(400)
+        .json({ msg: "You are already added in friend item" });
     } else {
-        return res.status(400).json({ msg: "No Friends" });
+      await friend_new.save();
+      const wallet = await Wallet.findOne({ username: req.body.username });
+      const wallet_friend = await Wallet.findOne({ username: req.body.friend });
+      const updated_wallet = await Wallet.findOneAndUpdate(
+        { username: req.body.username },
+        {
+          totalPoint: wallet.totalPoint + 1000,
+          balance: wallet.balance + 1000
+        }
+      );
+      const updated_wallet_friend = await Wallet.findOneAndUpdate(
+        { username: req.body.friend },
+        {
+          totalPoint: wallet_friend.totalPoint + 1000,
+          balance: wallet_friend.balance + 1000
+        }
+      );
+      res.json(friend_new);
     }
-})
-export default router
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+});
+router.post("/:username", async (req: Request, res: Response) => {
+  let friend = await Friend.find({ username: req.params.username });
+  if (friend) {
+    res.json(friend);
+  } else {
+    return res.status(400).json({ msg: "No Friends" });
+  }
+});
+export default router;
